7b73c65aad1378e0c49327454dab413089e70934
alessandro <alessandro.gianfelici@edison.it>
2020-10-14 11:04:18 +0200

    fix

--- toad/constants.py
+++ toad/constants.py
@@ -1,9 +1,10 @@
+import os
 # Oanda:
-oanda_token_practice = '7b18014e31b712d0d4479196659f0cdf-3a28a5d777903067dbb80bb7693f2dd4'
-accountID_practice = "101-004-9618527-001"
+oanda_token_practice = os.environ['OANDA_TOKEN_PRACTICE']
+accountID_practice = os.environ['ACCOUNTID_PRACTICE']
 
-accountID = '001-004-2388730-001'
-oanda_token = 'a9b5945cf12d54240860a6a66029c494-cf2505770460f37bff2e78d76250174a'
+accountID = os.environ['ACCOUNTID']
+oanda_token = os.environ['OANDA_TOKEN']
 
 # Percorso
 myPath = '/home/alessandro/Documenti/oanda_helper/data/'
